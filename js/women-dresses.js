470c68dfb1c8d48060ac5b2351064f4cefd6d849
Hafsa Khan Lodhi <hafsalodhi2023@gmail.com>
2023-10-29 11:23:55 +0500

    product page final

--- js/women-dresses.js
+++ js/women-dresses.js
@@ -1,112 +1,28 @@
 /*----------------------------
-CART MENU STARTED
+small images slider started
 ----------------------------*/
-const cartIcon = document.querySelector(".fa-cart-shopping");
-const cartContainer = document.querySelector(".cart-container");
-const crossIcon = document.querySelector(".fa-circle-xmark");
 
-cartIcon.addEventListener("click", function () {
-  cartContainer.style.left = "0%";
+const smallImages = document.querySelectorAll("#photo-change img");
+
+smallImages.forEach(smallImage => {
+    smallImage.addEventListener("click", function () {
+        let smallImagesParent = smallImage.parentElement;
+        let photoChangeDivParent = smallImagesParent.parentElement;
+        let photoChangeDivParentSibling = photoChangeDivParent.previousElementSibling;
+        let photoChangeDivParentSiblingChild = photoChangeDivParentSibling.children[0];
+
+        photoChangeDivParentSiblingChild.src = smallImage.src;
+  })  
 });
 
-crossIcon.addEventListener("click", function () {
-  cartContainer.style.left = "100%";
-});
 /*----------------------------
-CART MENU ENDED
+small images slider ended
 ----------------------------*/
 
 /*----------------------------
-CARDS DATA FETCHING LOGIC STARTED
+filter logic started
 ----------------------------*/
 
-let api = "json/women-dresses.json";
-let cardContainer = document.querySelector(".collection-div");
-
-fetch(api)
-  .then((res) => {
-    return res.json();
-  })
-  .then((jsonData) => {
-    let products = jsonData.description;
-
-    products.forEach((data) => {
-      cardContainer.append(
-        card_Func(
-          data.images,
-          data.productName,
-          data.productCode,
-          data.productSize,
-          data.productColor,
-          data.productPrice,
-          data.productDiscount,
-          data.productSellingPrice
-        )
-      );
-    });
-  });
-
-function card_Func(
-  images,
-  productName,
-  productCode,
-  productSizes,
-  productColors,
-  productPrice,
-  productDiscount,
-  productSellingPrice
-) {
-  let card = document.createElement("div");
-  card.className = "card";
-  card.innerHTML = `
-          <div class="card-left">${images.map(
-            (image) => `<img src="${image}" >`
-          )}</div>
-          <div class="card-right">
-            <h3>${productName}</h3>
-            <div><span>Product Code :</span><span>${productCode}</span></div>
-            <div>
-              <span>Size :</span
-              ><span
-                ><select name="" id="size">
-                ${productSizes.map(
-                  (size) => `<option value="${size}">${size}</option>`
-                )}
-                </select></span
-              >
-            </div>
-            <div>
-              <span>Color :</span
-              ><span
-                ><select name="" id="">
-                ${productColors.map(
-                  (color) => `<option value="${color}">${color}</option>`
-                )}
-                </select></span
-              >
-            </div>
-            <div><span>Price</span><span>${productPrice}</span></div>
-            <div><span>Discount:</span><span>${productDiscount}</span></div>
-            <div><span>Selling Price:</span><span>${productSellingPrice}</span></div>
-            <div id="photo-change">
-            <img src="images/women-dresses/product (3).jpg" alt="">
-            <img src="images/women-dresses/product (3).jpg" alt="">
-            <img src="images/women-dresses/product (3).jpg" alt="">
-            <img src="images/women-dresses/product (3).jpg" alt="">
-            </div>
-            <div id="add-cart-btn">
-              <a href=""
-                >ADD TO CART &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i
-                  class="fa-solid fa-cart-shopping fa-bounce"
-                  style="font-size: 14px"
-                ></i
-              ></a>
-            </div>
-          </div>
-  `;
-  return card;
-}
-
 /*----------------------------
-CARDS DATA FETCHING LOGIC ENDED
+filter logic ended
 ----------------------------*/
